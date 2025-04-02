35b0be9a0b100986e94afa99a14d0689bb6bd096
AdamRB208 <adamrborup@gmail.com>
2025-04-02 12:00:11 -0600

    saving listed car to local storage

--- app/controllers/CarsController.js
+++ app/controllers/CarsController.js
@@ -1,10 +1,14 @@
 import { AppState } from "../AppState.js"
+import { Car } from "../models/Car.js"
+import { carsService } from "../services/CarsService.js"
+import { getFormData } from "../utils/FormHandler.js"
 
 
 export class CarsController {
   constructor() {
     console.log('Cars Controller Running!')
     this.drawCars()
+    AppState.on('cars', this.drawCars)
   }
 
 
@@ -15,5 +19,22 @@ export class CarsController {
     cars.forEach(car => carsContent += car.CardTemplate)
     const listingElem = document.getElementById('listings')
     listingElem.innerHTML = carsContent
+  }
+
+  createCarListing() {
+    console.log('Cars Listing Running')
+    console.log(event);
+    event.preventDefault()
+    const form = event.target
+    console.log(form.make.value);
+    console.log(form);
+    // const formData = new FormData(form)
+    // console.log('data', formData);
+    // formData.forEach((value, name) => console.log(value, name))
+    const carData = getFormData(form)
+    console.log('car data', carData);
+    carsService.createCarListing(carData)
+    form.reset()
+
   }
 }
--- app/services/CarsService.js
+++ app/services/CarsService.js
@@ -1,7 +1,27 @@
+import { AppState } from "../AppState.js"
+import { Car } from "../models/Car.js"
 
 
 class CarsService {
+  createCarListing(carData) {
+    const car = new Car(carData)
+    console.log('new car', car)
+    // AppState.cars.push(car)
+    AppState.cars.unshift(car)
+    this.saveCars()
+  }
 
+  saveCars() {
+    const cars = AppState.cars
+    const carsString = JSON.stringify(cars)
+    console.log('cars string', carsString);
+    // console.log(JSON.parse(carsString));
+    localStorage.setItem('gregslist_cars', carsString)
+  }
+
+  loadCars() {
+
+  }
 
 }
 
